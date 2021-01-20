f4932d5467b44fcde5a32d347ed7b10310cc784a
Harvey Ayaay <harveyayaay@gmail.com>
2021-01-20 17:24:35 +0800

    Basic HTML Template

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,11 @@
+<div class="navbar">
+  <div class="navbar-inner">
+    <a id="logo" href="/">Gemini</a>
+    <ul class="nav">
+      <li><a href="/">Dashboard</a></li>
+      <li><a href="/about">Employee Management</a></li>
+      <li><a href="/projects">Projects</a></li>
+      <li><a href="/contact">Contact</a></li>
+    </ul>
+  </div>
+</div>
